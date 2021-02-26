f8edcc5a1a6fdb720c59fd84ee87da14183ab47e
dangvantho <dangvantho12as0@gmail.com>
2021-02-26 08:37:09 +0700

    Function components and Class components

---
+++ Lesson 4 Create react app/todo-App/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './components/Greeting'
+import ClassComponent from './components/ClassComponent'
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson 4 Create react app/todo-App/src/components/ClassComponent.js
@@ -0,0 +1,12 @@
+//Class COmponent
+import React,{Component} from 'react'
+class ClassComponent extends Component{
+    render(){
+        return (
+            <h2>
+               This is ClassComponent
+           </h2>
+        )
+    }
+}
+export default ClassComponent
