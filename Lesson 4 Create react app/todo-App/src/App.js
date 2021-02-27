89cd04f856acdd4fddd02835d6fc8db88ffebeba
dangvantho <dangvantho12as0@gmail.com>
2021-02-27 16:55:26 +0700

    Handling Events and Form React

--- Lesson 4 Create react app/todo-App/src/App.js
+++ Lesson 4 Create react app/todo-App/src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
 import Todo from './components/Todo'
-function App() {
-  const items=[
-    {isSmall:true , img: 'http://bit.ly/3ayPusa' },
-    {isSmall:false , img: 'http://bit.ly/3ayPusa' },
-    {isSmall:false , img: 'http://bit.ly/3ayPusa' },
-    {isSmall:false , img: 'http://bit.ly/3ayPusa' },
-    {isSmall:true , img: 'http://bit.ly/3ayPusa' },
-    {isSmall:false , img: 'http://bit.ly/3ayPusa' },
-  ]
-  return (
-    <div className="App">
-      {items.map((item,index)=>{
-        return (
-          <Todo isSmall={item.isSmall} image={item.img} key={index} />
-        )
-      })}
-    </div>
-  );
+import React, { Component } from 'react'
+const todoList=[
+  {id: 0, title:'Đi học',isComplete: false},
+  {id: 1, title:'Tập thể dục',isComplete: true},
+  {id: 2, title:'Đi ăn',isComplete: false}
+]
+class App extends Component {
+  constructor(){
+    super()
+    this.state={...todoList}
+  }
+  onClick(e){
+    let id=e.target.dataset.id
+    console.log(id)
+    if(todoList[id].isComplete) todoList[id].isComplete=false
+    else todoList[id].isComplete=true
+    console.log(todoList)
+    this.setState({...todoList})
+  }
+  createArray(obj){
+    let arr=[]
+    for(let i in obj){
+      arr.push(obj[i])
+    }
+    return arr
+  }
+  render(){
+    return (
+      <div className="App">
+        {this.createArray(this.state).map((item,index)=>{
+          return <Todo isComplete={item.isComplete} dataId={index} change={this.state.one} key={index}
+           onClick={this.onClick.bind(this)} >
+            {item.title}
+          </Todo>
+        })}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- Lesson 4 Create react app/todo-App/src/components/Todo.js
+++ Lesson 4 Create react app/todo-App/src/components/Todo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './Todo.css'
+import classNames from 'classnames/bind'
 class Todo extends Component {
     render() {
-        const isSmall=this.props.isSmall ? 'isSmall': ''
-        return (
-            <div className='col-3'>
-                <img className={isSmall}  src={this.props.image} />
+        const props=this.props
+        return(
+            <div className={classNames('todo',{done:props.isComplete})} >
+               <input type='checkbox' onClick={props.onClick} 
+                checked={props.isComplete} data-id={props.dataId} /> 
+               {props.children}
             </div>
         )
     }
